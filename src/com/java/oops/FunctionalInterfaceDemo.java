690370b7787e54956051934dc4ddc259e8beb8e1
akashgudadhe <akashgudadhe112@gmail.com>
2021-08-29 20:43:12 +0530

    functional interface demo

---
+++ src/com/java/oops/FunctionalInterfaceDemo.java
@@ -0,0 +1,21 @@
+package com.java.oops;
+
+//Types of interfaces
+/*
+ *  marker interface
+ *  single abstract method interface - functional interface - lambda expression
+ *  normal interface
+ */
+
+interface DemoInterface{
+	void show();
+}
+
+public class FunctionalInterfaceDemo {
+
+	public static void main(String[] args) {
+		DemoInterface f = () -> System.out.println("Functional interface demo");
+		
+		f.show();
+	}
+}
