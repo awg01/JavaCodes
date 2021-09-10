ff4b1673a3bc1996b5d3e1e46c5536a6253bf0d5
akashgudadhe <akashgudadhe112@gmail.com>
2021-09-10 11:53:05 +0530

    String immutablity exercise

---
+++ src/com/java/string/StringDemo1.java
@@ -0,0 +1,25 @@
+package com.java.string;
+
+public class StringDemo1 {
+   public static void main(String[] args) {
+	 String s = "abcd";
+	 String s2 = s;
+	 
+	 s = s.concat("Hello");
+	 System.out.println(s);
+	 System.out.println(s2);
+	 
+	 String s3 = "Hello";
+	 s3.concat("acd");
+	 System.out.println(s3);
+	 
+	 String s4 = s3;
+	 String s5 = "Hello";
+	 
+	 if( s3==s4 && s4==s5 ) {
+		 System.out.println( "true" );
+	 }
+	 
+	 
+   }
+}
