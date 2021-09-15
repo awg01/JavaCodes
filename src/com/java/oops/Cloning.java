cdf56181c7a27ff1b9d32724efecd7a3e4833fb1
akashgudadhe <akashgudadhe112@gmail.com>
2021-09-15 23:15:20 +0530

    cloneable interface example

---
+++ src/com/java/oops/Cloning.java
@@ -0,0 +1,32 @@
+package com.java.oops;
+
+public class Cloning {
+
+	public static void main(String[] args) throws CloneNotSupportedException {
+        Child obj = new Child();
+        obj.i = 5;
+        obj.j=6;
+     
+        Child  obj1 = (Child) obj.clone();
+
+        
+        System.out.println("obj1 "+obj1); //5 6
+        System.out.println("obj "+obj);   //5 6
+	}
+}
+
+class Child implements Cloneable{
+	int i;
+	int j;
+	
+	@Override
+	public String toString() {
+		return "Children [i=" + i + ", j=" + j + "]";
+	}
+	
+	@Override
+	public Object clone() throws CloneNotSupportedException{
+		return super.clone();
+	}
+}
+
